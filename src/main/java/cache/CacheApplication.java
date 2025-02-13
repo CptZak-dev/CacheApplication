f75980b503909294fbc49328e76363a7adfbc22f
zakaria <zakaria.benami@gmail.com>
2025-02-13 21:25:35 +0100

    Ajout swagger

--- src/main/java/cache/CacheApplication.java
+++ src/main/java/cache/CacheApplication.java
@@ -1,8 +1,11 @@
 package cache;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "API Documentation", version = "1.0", description = "Documentation des API"))
 @SpringBootApplication
 public class CacheApplication {
     public static void main(String[] args) {
